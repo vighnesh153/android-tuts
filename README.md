72584c9b837484f096527e9de962ceed48a9bad9
rvighnesh <rvighnesh@google.com>
2024-02-07 12:10:12 +0530

    feat: initialize TriviaApp

--- README.md
+++ README.md
@@ -3,4 +3,5 @@
 1. DiceRoller: Build a basic android application
 2. AboutMe: A simple app to learn about layout
 3. ColorMyViews: An app to learn about constraint_layouts
+4. TriviaApp: An app to learn about app navigation (activities and fragments)
 
---
+++ TriviaApp/app/src/main/java/com/example/triviaapp/GameOverFragment.kt
@@ -0,0 +1,22 @@
+package com.example.triviaapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.example.triviaapp.databinding.FragmentGameOverBinding
+
+class GameOverFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding: FragmentGameOverBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_game_over, container, false
+        )
+        return binding.root
+    }
+}
