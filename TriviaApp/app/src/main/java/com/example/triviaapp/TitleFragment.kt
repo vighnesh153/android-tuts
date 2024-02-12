d592a7fd23473ddabc11cee8265fbf79b5f14fd9
rvighnesh <rvighnesh@google.com>
2024-02-12 11:09:31 +0530

    feat: navigation setup in Trivia App

---
+++ TriviaApp/app/src/main/java/com/example/triviaapp/TitleFragment.kt
@@ -0,0 +1,52 @@
+package com.example.triviaapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import com.example.triviaapp.databinding.FragmentTitleBinding
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [TitleFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class TitleFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val binding: FragmentTitleBinding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_title, container, false
+        )
+//        binding.playButton.setOnClickListener {
+//            it.findNavController().navigate(R.id.action_titleFragment_to_gameFragment)
+//        }
+        binding.playButton.setOnClickListener(
+            Navigation.createNavigateOnClickListener(R.id.action_titleFragment_to_gameFragment)
+        )
+        return binding.root
+    }
+
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment TitleFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            TitleFragment().apply {
+
+            }
+    }
+}
